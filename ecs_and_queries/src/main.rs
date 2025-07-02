b4e13df674234236dd1a60c74d54fa2b57815437
v3tto <022000649@upsrj.edu.mx>
2025-07-02 15:46:38 -0600

    bevy_test to ecs_and_queries, also added plugins_and_resources and comments

--- ecs_and_queries/src/main.rs
+++ ecs_and_queries/src/main.rs
@@ -3,18 +3,21 @@ use bevy::prelude::*;
 fn hello_world() {
     println!("hello world!");
 }
+
 #[derive(Component)]
 struct Person;
 
 #[derive(Component)]
 struct Name(String);
 
+// adds entities to the "world"
 fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Vin Venture".to_string())));
     commands.spawn((Person, Name("Stuart Harold Pot".to_string())));
     commands.spawn((Person, Name("Soul Evans".to_string())));
 }
 
+// this queries all entities with name and person
 fn greet_people(query: Query<&Name, With<Person>>) {
     for name in &query {
         println!("hello {}!", name.0);
@@ -22,6 +25,7 @@ fn greet_people(query: Query<&Name, With<Person>>) {
 }
 
 fn update_people(mut query: Query<&mut Name, With<Person>>) {
+    // the for loop makes your there is one mutable reference at once
     for mut name in &mut query {
         if name.0 == "Soul Evans" {
             name.0 = "Soul Eater".to_string();
@@ -32,7 +36,7 @@ fn update_people(mut query: Query<&mut Name, With<Person>>) {
 
 fn main() {
     App::new()
-        .add_systems(Startup, add_people)
-        .add_systems(Update, (hello_world, (update_people, greet_people).chain()))
+        .add_systems(Startup, add_people) // makes sure this system runs at the start
+        .add_systems(Update, (hello_world, (update_people, greet_people).chain())) // chain() makes systems run in a sequence
         .run();
 }
---
+++ plugins_and_resources/src/main.rs
@@ -0,0 +1,49 @@
+use bevy::prelude::*;
+
+fn hello_world() {
+    println!("hello world!");
+}
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Vin Venture".to_string())));
+    commands.spawn((Person, Name("Stuart Harold Pot".to_string())));
+    commands.spawn((Person, Name("Soul Evans".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0);
+    }
+}
+
+fn update_people(mut query: Query<&mut Name, With<Person>>) {
+    for mut name in &mut query {
+        if name.0 == "Soul Evans" {
+            name.0 = "Soul Eater".to_string();
+            break;
+        }
+    }
+}
+
+pub struct HelloPlugin;
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, add_people);
+        app.add_systems(Update, (hello_world, (update_people, greet_people).chain()));
+        
+    }
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins) //this adds things like WindowPlugin and the event loop
+        .add_plugins(HelloPlugin)
+        // .add_systems(Startup, add_people)
+        // .add_systems(Update, (hello_world, (update_people, greet_people).chain()))
+        .run();
+}
