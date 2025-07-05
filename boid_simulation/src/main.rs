7c2f5fec1b3ce3d451414321dd042704b8f134dd
v3tto <022000649@upsrj.edu.mx>
2025-07-05 12:07:20 -0600

    naive_detection added (not optimal)

--- boid_simulation/src/main.rs
+++ boid_simulation/src/main.rs
@@ -1,11 +1,11 @@
-use bevy::prelude::*;
+use bevy::{color::palettes::css::BLUE, prelude::*};
 use rand::Rng;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
-        .add_systems(Update, boid_movement)
+        .add_systems(Update, (boid_movement, naive_detection))
         .run();
 }
 
@@ -22,9 +22,9 @@ fn setup(
     commands.spawn(Camera2d);
 
     let triangle = meshes.add(Triangle2d::new(
-        Vec2::Y * 20.0,
-        Vec2::new(-10.0, -20.0),
-        Vec2::new(10.0, -20.0),
+        Vec2::Y * 10.0,
+        Vec2::new(-5.0, -10.0),
+        Vec2::new(5.0, -10.0),
     ));
     let color = Color::Srgba(Srgba::new(1.0, 0.0, 0.27, 1.0));
     let material = materials.add(color);
@@ -38,7 +38,7 @@ fn setup(
                 Mesh2d(triangle.clone()),
                 MeshMaterial2d(material.clone()),
                 Transform::from_xyz(x, y, 0.0),
-                Boid { velocity: 100.0 },
+                Boid { velocity: 50.0 },
             )
         })
         .collect::<Vec<_>>();
@@ -52,5 +52,26 @@ fn boid_movement(time: Res<Time>, mut query: Query<(&Boid, &mut Transform)>) {
         let velocity = forward * boid.velocity * time.delta_secs();
 
         transform.translation += velocity;
+    }
+}
+
+fn naive_detection(mut gizmos: Gizmos, query: Query<&Transform, With<Boid>>) {
+    for self_transform in &query {
+        for other_transfrom in &query {
+            if self_transform.translation == other_transfrom.translation {
+                continue;
+            }
+            let distance = self_transform
+                .translation
+                .distance(other_transfrom.translation);
+            if distance < 50.0 {
+                gizmos.line(
+                    self_transform.translation.trunc(),
+                    other_transfrom.translation.trunc(),
+                    BLUE,
+                );
+            }
+        }
+        gizmos.circle_2d(self_transform.translation.truncate(), 50.0, BLUE);
     }
 }
